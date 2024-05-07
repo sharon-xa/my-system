bdb5375c675d51d35850bfd230d316135b77f991
sharon <ali93456@gmail.com>
2024-05-07 03:30:28 +0300

    format script

--- fonts/installFiraCodeFont.sh
+++ fonts/installFiraCodeFont.sh
@@ -5,31 +5,27 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-if ! command -v wget &> /dev/null
-then
-    echo -e "${RED}wget could not be found"
-    echo -e "${RED}please install wget and try again."
-    exit 1
+if ! command -v wget &>/dev/null; then
+	echo -e "${RED}wget could not be found"
+	echo -e "${RED}please install wget and try again."
+	exit 1
 fi
 
-if ! command -v unzip &> /dev/null
-then
-    echo -e "${RED}unzip could not be found"
-    echo -e "${RED}please install unzip and try again."
-    exit 1
+if ! command -v unzip &>/dev/null; then
+	echo -e "${RED}unzip could not be found"
+	echo -e "${RED}please install unzip and try again."
+	exit 1
 fi
 
-if ! command -v fc-cache &> /dev/null
-then
-    echo -e "${RED}unzip could not be found"
-    echo -e "${RED}please install fc-cahce and try again."
-    exit 1
+if ! command -v fc-cache &>/dev/null; then
+	echo -e "${RED}unzip could not be found"
+	echo -e "${RED}please install fc-cahce and try again."
+	exit 1
 fi
-
 
 if ! [ -f ~/Downloads/FiraCode.zip ]; then
-  echo "Downloading FiraCode Font..."
-  wget -P ~/Downloads/ https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip
+	echo "Downloading FiraCode Font..."
+	wget -P ~/Downloads/ https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip
 fi
 
 unzip ~/Downloads/FiraCode.zip -d ~/Downloads/FiraCode/
