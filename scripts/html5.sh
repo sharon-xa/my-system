c91f9e26074f28cd3347ed3efd8a34b52cc65b9b
sharon <ali93456@gmail.com>
2024-07-04 04:07:58 +0300

    add new script

---
+++ scripts/html5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <title></title>
+    <link href=\"css/style.css\" rel=\"stylesheet\">
+  </head>
+  <body>
+  
+  </body>
+</html>"
